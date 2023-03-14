4783e774893fa391db497caa3dc07345ab988c1d
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-14 12:09:25 +0100

    CHANGE Output messages

--- snack-2/assets/js/main.js
+++ snack-2/assets/js/main.js
@@ -12,8 +12,8 @@ if (firstWord.length == 0 || secondWord.length == 0) {
   if (firstWord.length == secondWord.length) {
     console.log("The two words have the same length");
   } else if (firstWord.length > secondWord.length) {
-    console.log(`The first word "${firstWord}" is longer than the second "${secondWord}"`)
+    console.log(`The first word "${firstWord}" is longer than the second "${secondWord}" so the right order is: ${firstWord}, ${secondWord}`)
   } else {
-    console.log(`The second word "${secondWord}" is longer than the first "${firstWord}"`)
+    console.log(`The second word "${secondWord}" is longer than the first "${firstWord}" so the right order is: ${secondWord}, ${firstWord}`)
   }
 }
