640e707dd7de10b472bb2fede9cc787fec188615
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-14 11:43:02 +0100

    ADD Control over input

--- snack-1/assets/js/main.js
+++ snack-1/assets/js/main.js
@@ -8,6 +8,10 @@
 const firstNumber = Number(prompt("Insert the first number"));
 const secondNumber = Number(prompt("Insert the second number"));
 
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+  alert("You should insert two numbers");
+}
+
 if (firstNumber === secondNumber) {
   console.log("The inserted numbers are the same");
 } else if (firstNumber > secondNumber) {
